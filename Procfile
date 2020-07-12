cb5e35dadbeac6003eb3b165ff90680de1b14d68
Carl Yebes <yebes77gmail.com>
2020-07-12 17:45:00 +0800

    added db

--- Procfile
+++ Procfile
@@ -4,4 +4,5 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
 
 web: gunicorn SecondWebsite.wsgi --log-file -
+release: python manage.py makemigrations
 release: python manage.py migrate --noinput
--- dentalwebsite/models.py
+++ dentalwebsite/models.py
@@ -26,13 +26,6 @@ class Form_two(models.Model):
     def __str__(self):
         return self.name
 
-class Form_three(models.Model):
-    name = models.CharField(max_length=50)
-    number = models.IntegerField()
-    email = models.EmailField()
-
-    def __str__(self):
-        return self.name
 
 class Form_four(models.Model):
     name = models.CharField(max_length=50)
