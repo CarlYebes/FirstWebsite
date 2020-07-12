54489c9449b5668059edf7ca26f1537c9d8f2cf9
Carl Yebes <yebes77gmail.com>
2020-07-12 17:35:09 +0800

    added databases

--- dentalwebsite/admin.py
+++ dentalwebsite/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Form
+from .models import Form, Form_one, Form_two
 # Register your models here.
 
 admin.site.register(Form)
+admin.site.register(Form_one)
+admin.site.register(Form_two)
--- dentalwebsite/models.py
+++ dentalwebsite/models.py
@@ -18,3 +18,12 @@ class Form_one(models.Model):
     def __str__(self):
         return self.name
 
+class Form_two(models.Model):
+    name = models.CharField(max_length=50)
+    number = models.IntegerField()
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+
