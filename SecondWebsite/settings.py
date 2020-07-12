805f0b32d929f5621dc651cfa8638edbccb123c0
Carl Yebes <yebes77gmail.com>
2020-07-12 17:54:32 +0800

    delete

--- SecondWebsite/settings.py
+++ SecondWebsite/settings.py
@@ -131,8 +131,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'yebes77@gmail.com'
-EMAIL_HOST_PASSWORD = 'Yebes!123'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 django_heroku.settings(locals())
