f9bf7151d593fa60fac7ba03f1b722a25856a869
Carl Yebes <yebes77gmail.com>
2020-07-12 17:23:28 +0800

    added postgresql

--- SecondWebsite/settings.py
+++ SecondWebsite/settings.py
@@ -80,8 +80,7 @@ WSGI_APPLICATION = 'SecondWebsite.wsgi.application'
 
 DATABASES = {
 }
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
